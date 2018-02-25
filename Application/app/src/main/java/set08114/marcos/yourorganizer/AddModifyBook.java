bb24fb9cf19fc359008c346ee5f9cdbbd840691c
MarcosGaming <40284094@live.napier.ac.uk>
2018-02-25 22:56:46 +0000

    ok

--- Application/app/src/main/java/set08114/marcos/yourorganizer/AddModifyBook.java
+++ Application/app/src/main/java/set08114/marcos/yourorganizer/AddModifyBook.java
@@ -1,22 +1,84 @@
 package set08114.marcos.yourorganizer;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class AddModifyBook extends AppCompatActivity {
+import static android.widget.Toast.LENGTH_SHORT;
+
+public class AddModifyBook extends AppCompatActivity
+{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_modify_book);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Button addBtn = findViewById(R.id.addBtn);
+        addBtn.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View view)
+            {
+                EditText nameText = findViewById(R.id.nameEditText);
+                EditText chapterText = findViewById(R.id.chapterEditText);
+                EditText pageText = findViewById(R.id.pageEditText);
+                //The user must enter at least the name
+                if(nameText.getText().toString().isEmpty())
+                {
+                    String message = "Please enter a name";
+                    Toast.makeText(getApplicationContext(), message, LENGTH_SHORT).show();
+                }
+                else
+                {
+                    Book book = new Book();
+                    book.setName(nameText.getText().toString());
+                    book.setChapter(Integer.parseInt(chapterText.getText().toString()));
+                    book.setPage(Integer.parseInt(pageText.getText().toString()));
+                    //Check if one of the check boxes is checked
+                    CheckBox completedCheckBox = findViewById(R.id.completedCheckBox);
+                    CheckBox onGoingCheckBox = findViewById(R.id.onGoingCheckBox);
+                    if(completedCheckBox.isChecked())
+                    {
+                        book.setStatus(completedCheckBox.getText().toString());
+                    }
+                    else if(onGoingCheckBox.isChecked())
+                    {
+                        book.setStatus(onGoingCheckBox.getText().toString());
+                    }
+                    //Add book to storage
+
+                }
+            }
+        });
     }
 
+    public void onCheckboxClicked(View view)
+    {
+
+        CheckBox completedCheckBox = findViewById(R.id.completedCheckBox);
+        CheckBox onGoingCheckBox = findViewById(R.id.onGoingCheckBox);
+
+        // Check which checkbox was clicked
+        switch(view.getId())
+        {
+            case R.id.completedCheckBox:
+                completedCheckBox.setChecked(true);
+                onGoingCheckBox.setChecked(false);
+                break;
+            case R.id.onGoingCheckBox:
+                onGoingCheckBox.setChecked(true);
+                completedCheckBox.setChecked(false);
+                break;
+        }
+    }
 }
--- Application/app/src/main/java/set08114/marcos/yourorganizer/InternalStorage.java
+++ Application/app/src/main/java/set08114/marcos/yourorganizer/InternalStorage.java
@@ -7,16 +7,38 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.util.List;
 
 /**
  * Created by Marcos on 14/02/2018.
+ * Singleton Pattern Used
  */
 
 public class InternalStorage {
 
-    private InternalStorage(){}
+    private InternalStorage instance;
+    private List<Book> bookList;
+    private List<Film> filmList;
+    private List<Serie> serieList;
 
-    public static void writeObject(Context context, String key, Object object)
+    //Singleton Constructor
+    private InternalStorage(){}
+    //Accessor that only allows to get the singleton instance
+    public static InternalStorage getInstance
+    {
+        if(instance == null)
+        {
+            instance = new InternalStorage();
+
+        }
+    }
+
+    private static void initialiseLists()
+    {
+        List<Book> books = readObject(,"books");
+    }
+
+    private static void writeObject(Context context, String key, Object object)
         throws IOException {
         FileOutputStream fos = context.openFileOutput(key, Context.MODE_PRIVATE);
         ObjectOutputStream oos = new ObjectOutputStream(fos);
@@ -24,7 +46,7 @@ public class InternalStorage {
         oos.close();
         fos.close();
     }
-    public static Object readObject(Context context, String key) throws IOException,
+    private static Object readObject(Context context, String key) throws IOException,
             ClassNotFoundException {
         FileInputStream fis = context.openFileInput(key);
         ObjectInputStream ois = new ObjectInputStream(fis);
